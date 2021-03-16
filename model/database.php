29394cfa0649e8cd4dc6982fd0ce4d52ac464045
Eugene <e_kramer2@mail.fhsu.edu>
2021-03-15 22:25:07 -0700

    Added.

--- model/database.php
+++ model/database.php
@@ -2,12 +2,16 @@
     //$dsn = 'mysql:host=localhost;dbname=zippyusedautos';
     //$username = "root";//'mgs_user';
     //$password = "";//'pa55word';
-    $dsn = getenv("JAWSDB_URL");
+    $host       = getenv('JAW_HOST');
+    $dbname     = getenv('JAW_DB');
+    $dsn        = "mysql:host={$host};dbname={$dbname}";
+    $username   = getenv('JAW_USER');
+    $password   = getenv('JAW_PASS');
 
     $db = null;
 
     try {
-        $db = new PDO($dsn);//, $username, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('database_error.php');
